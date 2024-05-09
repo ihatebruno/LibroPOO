7afcd42bee01b248ac514927f985f9aee4b22a94
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-05-09 16:50:47 -0600

    Libro POO comentado

--- Libro.java
+++ Libro.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-
 public class Libro {
     private String editorial;
     private String titulo;
     private String autor;
     private int numeroPaginas;
     
+    // Constructor que inicializa todas las propiedades del libro.
     public Libro(String editorial, String titulo, String autor, int numeroPaginas) {
         this.editorial = editorial;
         this.titulo = titulo;
@@ -13,9 +12,11 @@ public class Libro {
         this.numeroPaginas = numeroPaginas;
     }
     
+    // Constructor vacío.
     public Libro() {
     }
 
+    // Método para cargar los detalles del libro desde la entrada del usuario.
     public void cargarLibro() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa la editorial del libro:");
@@ -26,14 +27,13 @@ public class Libro {
         autor = sc.nextLine();
         System.out.println("Ingresa el número de páginas del libro:");
         numeroPaginas = sc.nextInt();
-        
     }
     
+    // Método para imprimir los detalles del libro.
     public void informarDetalles() {
         System.out.println("Editorial: " + editorial);
         System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("Número de páginas: " + numeroPaginas);
     }
-    
 }
--- MainLibro.java
+++ MainLibro.java
@@ -1,8 +1,8 @@
 public class MainLibro {
     public static void main(String[] args) {
-    Libro libro1 = new Libro();
-    libro1.cargarLibro();
-    System.out.println("---Detalles del libro---");
-    libro1.informarDetalles();
-}
+        Libro libro1 = new Libro(); // Crear un objeto Libro.
+        libro1.cargarLibro(); // Llamar al método cargarLibro para que el usuario ingrese los detalles del libro.
+        System.out.println("---Detalles del libro---");
+        libro1.informarDetalles(); // Imprimir los detalles del libro.
+    }
 }
